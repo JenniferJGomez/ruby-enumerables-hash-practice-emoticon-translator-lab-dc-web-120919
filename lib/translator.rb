a5938f3fd255c89735a1f8c06514720e30b3c946
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-11-19 17:41:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,17 @@ require 'yaml'
 
 def load_library(file)
  emotes = YAML.load_file(file)
- 
+  mean_hash = {}
+  emo_hash = {}
+  x = 0
+  emotes.values.each do |group|
+    america = group[0]
+    japan = group[1]
+    mean_hash[japan] = emotes.keys[x]
+    emo_hash[america] = japan
+    x += 1
+  end
+  [[:get_meaning, mean_hash], [:get_emoticon, emo_hash]].to_h
   
 end
 
